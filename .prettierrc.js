17eb94219bcfa352787618fedd24222dc40ecd9c
Manbeartrain <ahigareda15@gmail.com>
2022-10-28 21:29:52 -0600

    Initialized Project

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    arrowParens: "avoid",
+    bracketSameLine: true,
+    bracketSpacing: false,
+    singleQuote: false,
+    semi: true,
+    printWidth: 80,
+    tabWidth: 4,
+};
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+    return (
+        <div className="w-full h-[100vh] flex justify-center items-center">
+            React Authentication tutorial
+        </div>
+    );
+}
+
+export default App;
